013b9279c17c69bf773ec76e6c81279c8f4df2c4
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-12-15 14:48:40 +0100

    Add RWD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './styles/style.scss';
+import a from './assets/a.jpg';
+import b from './assets/b.jpg';
+import c from './assets/c.jpg';
+import d from './assets/d.jpg';
+import facebook from './assets/facebook.png';
+import instagram from './assets/instagram.png';
+import youtube from './assets/youtube.png';
+import pinterest from './assets/pinterest.png';
+
 import getFooterYear from './scripts/footerDate';
 import pickWorkshop from './scripts/workshopPick';
-
 
 document.addEventListener('DOMContentLoaded', function() {
   getFooterYear();
